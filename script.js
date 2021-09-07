92e94c38387d2dfdf0eb0b11b004737292c6d110
Vigneshwaran230 <85312813+Vigneshwaran230@users.noreply.github.com>
2021-09-07 09:43:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const info = document.createElement("div");
+info.setAttribute("class", "userDetails");
+document.body.append(info);
+async function getimage() {
+  const data = await fetch("https://dog.ceo/api/breeds/image/random");
+  const deatils = await data.json();
+  console.log(deatils.message);
+  document.querySelector(".userDetails").innerHTML='';
+  createuserdeatails(deatils.message);
+}
+getimage();
+
+function createuserdeatails(img) {
+  const infor = document.createElement("div");
+  infor.setAttribute("class", "container");
+  infor.innerHTML = `
+  <div class="button1">
+  <button class="btn" onclick="moveimages()">Next-image</button>
+  </div>
+  <div class="cat-container">
+  <img class="cat-pic" src=${img} >
+  </div>
+  `;
+  document.querySelector(".userDetails").append(infor);
+}
+
+function moveimages(){
+  getimage();
+}
